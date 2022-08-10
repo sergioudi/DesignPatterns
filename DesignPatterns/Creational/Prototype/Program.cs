17b806e102a6992a697d7be2b667ce5e75b584d2
Sergio Ricardo de Sousa <sergio.ricardo.sousa@avanade.com>
2022-08-10 13:54:20 -0300

    feat(Creational): add project Prototype

---
+++ DesignPatterns/Creational/Prototype/Program.cs
@@ -0,0 +1,17 @@
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+var manager = new Manager("Cindy");
+var managerClone = (Manager?)manager.Clone();
+Console.WriteLine($"Manager was cloned: {managerClone?.Name}");
+
+var employee = new Employee("Kevin", manager);
+var employeeClone = (Employee?)employee.Clone(true);
+Console.WriteLine($"Employee was cloned: {employeeClone?.Name}, with manager {employeeClone?.Manager.Name}");
+
+// change the manager name
+manager.Name = "Karen"; 
+Console.WriteLine($"Employee was cloned: {employeeClone?.Name}, with manager {employeeClone?.Manager.Name}");
+
+Console.ReadKey();
