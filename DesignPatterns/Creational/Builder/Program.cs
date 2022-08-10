7c3f381c311c8249f0ae2abb0642a315c46980c8
Sergio Ricardo de Sousa <sergio.ricardo.sousa@avanade.com>
2022-08-10 11:29:59 -0300

    feat(Creational): add project Builder

---
+++ DesignPatterns/Creational/Builder/Program.cs
@@ -0,0 +1,33 @@
+﻿using BuilderPattern;
+
+Console.Title = "BuilderPattern";
+
+Director director = new Director();
+
+Builder b1 = new ConcreteBuilder1();
+Builder b2 = new ConcreteBuilder2();
+
+// Construct two products
+director.Construct(b1);
+Product p1 = b1.GetResult();
+p1.Show();
+
+director.Construct(b2);
+Product p2 = b2.GetResult();
+p2.Show();
+
+
+Console.WriteLine("");
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+garage.Show();
+
+garage.Construct(bmwBuilder);
+garage.Show();
+ 
+Console.ReadKey();
