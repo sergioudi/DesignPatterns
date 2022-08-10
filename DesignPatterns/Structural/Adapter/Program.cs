455c006cb85cef1fd7cc69a89e8c95a819bac317
Sergio Ricardo de Sousa <sergio.ricardo.sousa@avanade.com>
2022-08-10 15:12:44 -0300

    feat(Structural): add project adapter

---
+++ DesignPatterns/Structural/Adapter/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.Title = "Adapter";
+{
+    Console.WriteLine("Using Adapter.ClassAdapter");
+
+    // adapter example (same code for object & class adapter)
+    Adapter.ClassAdapter.ICityAdapter adapter = new Adapter.ClassAdapter.CityAdapter();
+    var city = adapter.GetCity();
+
+    Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
+}
+
+{
+    Console.WriteLine("\nUsing Adapter.ObjectAdapter");
+
+    // adapter example (same code for object & class adapter)
+    Adapter.ObjectAdapter.ICityAdapter adapter = new Adapter.ObjectAdapter.CityAdapter();
+    var city = adapter.GetCity();
+
+    Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
+}
+
+Console.ReadKey(); 
