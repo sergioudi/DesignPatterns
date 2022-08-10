9f28189dc9f71d9c5ad947896f5e7206ddd93041
Sergio Ricardo de Sousa <sergio.ricardo.sousa@avanade.com>
2022-08-10 13:29:33 -0300

    feat(Creational): add project FactoryMethod

---
+++ DesignPatterns/Creational/FactoryMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using FactoryMethod;
+
+Console.Title = "FactoryMethod";
+
+var factories = new List<DiscountFactory> {
+    new CodeDiscountFactory(Guid.NewGuid()), 
+    new CountryDiscountFactory("BE") };
+
+foreach (var factory in factories)
+{
+    var discountService = factory.CreateDiscountService();
+    Console.WriteLine($"Percentage {discountService.DiscountPercentage} coming from {discountService}");
+} 
+
+Console.ReadKey();
